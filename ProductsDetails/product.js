f010f537488270f9b0e5e4b9174507ef0d7113e0
Sergio Njeim <70179273+freehotspot@users.noreply.github.com>
2024-05-01 15:03:49 +0000

    Co-authored-by: Angelokh22 <Angelokh22@users.noreply.github.com>

--- ProductsDetails/product.js
+++ ProductsDetails/product.js
@@ -3,7 +3,7 @@ var SmallImg = document.getElementsByClassName("small-img");
 
 SmallImg[0].onclick = function()
 {
-    ProductImg.src = SmallImg[0].src;
+    ProductImg.src = SmallImg[0].src;    
 }   
 SmallImg[1].onclick = function()
 {
