eeb055ac5d4e9490078677ee29d1422970cd1fd0
TaxEvaderer <164672360+TaxEvaderer@users.noreply.github.com>
2024-05-04 02:33:24 +0300

    Add files via upload

---
+++ signup/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var togglePassword = document.querySelectorAll(".toggle-password");
+
+  togglePassword.forEach(function(element) {
+      element.addEventListener("click", function() {
+          var input = document.querySelector(this.getAttribute("toggle"));
+          if (input.getAttribute("type") === "password") {
+              input.setAttribute("type", "text");
+          } else {
+              input.setAttribute("type", "password");
+          }
+          this.classList.toggle("fa-eye");
+          this.classList.toggle("fa-eye-slash");
+      });
+  });
+});
+
