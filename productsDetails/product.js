48bb8c9b7ed45f74e330e01525608476fcebf6c1
Simon KHALIL <160236663+PSIK999@users.noreply.github.com>
2024-05-07 11:53:07 +0000

    modified:   productsDetails/product.html 	modified:   productsDetails/product.js

--- productsDetails/product.js
+++ productsDetails/product.js
@@ -1,15 +1,15 @@
 var ProductImg = document.getElementById("ProductImg");
 var SmallImg = document.getElementsByClassName("small-img");
 
-SmallImg[0].onclick = function () {
+SmallImg[0].onmouseover = function () {
   ProductImg.src = SmallImg[0].src;
 };
-SmallImg[1].onclick = function () {
+SmallImg[1].onmouseover = function () {
   ProductImg.src = SmallImg[1].src;
 };
-SmallImg[2].onclick = function () {
+SmallImg[2].onmouseover = function () {
   ProductImg.src = SmallImg[2].src;
 };
-SmallImg[3].onclick = function () {
+SmallImg[3].onmouseover = function () {
   ProductImg.src = SmallImg[3].src;
 };
