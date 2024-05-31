be9be58a1cefc2ea92e04b43cb2d06a855ab3489
Simon KHALIL <160236663+PSIK999@users.noreply.github.com>
2024-05-31 14:57:08 +0300

    Add files via upload

--- signup/connect.php
+++ signup/connect.php
@@ -11,11 +11,12 @@ try {
     echo "Connection failed: " .$e->getMessage();
 }
 */
-$host="mysql-3974ab90-sergionjeim-e9a0.d.aivencloud.com:22356";
+$host="mysql-3974ab90-sergionjeim-e9a0.d.aivencloud.com";
+$port = "22356";
 $user="avnadmin";
 $pass="AVNS_lb6GguxdpmlAqBN023C";
 $db="saheltronix";
-  $conn=new mysqli($host,$user,$pass,$db);
+  $conn=new mysqli($host,$user,$pass,$db,$port);
   if($conn->connect_error){
      echo "Failed to connect DB".$conn->connect_error;
     }
