7d371d1f4cba825192c429e64011997289db822b
Sergio Njeim <70179273+freehotspot@users.noreply.github.com>
2024-06-04 23:39:01 +0300

    Add files via upload

---
+++ signup/regular_auth.php
@@ -0,0 +1,19 @@
+<?php
+include ("connect.php");;
+
+// session_start();
+
+function isLoggedIn() {
+    if (isset($_SESSION['email'] )) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+$loggedIn = isLoggedIn();
+$_SESSION['log'] = $loggedIn;
+
+
+?>
