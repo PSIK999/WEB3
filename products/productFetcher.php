0d57342450b1e9bd2e285312ce6f5182194ec575
TaxEvaderer <164672360+TaxEvaderer@users.noreply.github.com>
2024-06-05 20:11:57 +0300

    Update productFetcher.php

--- products/productFetcher.php
+++ products/productFetcher.php
@@ -1,3 +1,4 @@
+
 <?php
 
 function getProductDetails($conn, $product_id) {
@@ -10,16 +11,21 @@ function getProductDetails($conn, $product_id) {
         $row = $result->fetch_assoc();
         $product_html = "
             <div class='col-4'>
-                <img src='" . htmlspecialchars($row['image_url']) . "' />
-                <a href='../productsDetails/product.php'>
-                    <h4>" . htmlspecialchars($row['name']) . "</h4>
+                <img src='" . htmlspecialchars($row['image_url']) .  "' alt= '" . htmlspecialchars($row['description']) . "'  />
+                <div id='myForm'>
+                    <input type='hidden' name='product_id' value='" . htmlspecialchars($product_id) . "' />
+                    <input type='hidden' name='name' value='" . htmlspecialchars($row['name']) . "' />
+                    <input type='hidden' name='description' value='" . htmlspecialchars($row['description']) . "' />
+                    <input type='hidden' name='image_url' value='" . htmlspecialchars($row['image_url']) . "' />
+                    <input type='hidden' name='price' value='" . htmlspecialchars($row['price']) . "' />
+                    <a href='../productsDetails/product.php'>
+                        <h4>" . htmlspecialchars($row['name']) . "</h4>
+                    </a>
                     <div class='rating'>
-                        <a href='../checkout/checkout.php'>
-                            <button type='submit' class='buybtn'>Buy Now</button>
-                        </a>
+                        <button class='buybtn' onclick='addToCart(this)'>Add to Cart</button>
                     </div>
                     <p>$" . number_format($row['price'], 2) . "</p>
-                </a>
+                </div>
             </div>";
 
         return $product_html;
@@ -27,5 +33,22 @@ function getProductDetails($conn, $product_id) {
     } else {
         return "Product not found.";
     }
+
 }
 ?>
+  <script>
+    function addToCart(button) {
+      var item = button.parentNode.parentNode;
+      var productID = item.getElementsByTagName("input")[0].value;
+
+        fetch(
+          "../cart/cart.php",
+          {
+            method: 'POST',
+            body: new URLSearchParams({
+              product_id: productID
+            })
+          }
+        )
+    }
+  </script>
