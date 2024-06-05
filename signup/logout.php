a809a6820a38b5e6f909b6c76aa142690b6cb6f7
Sergio Njeim <70179273+freehotspot@users.noreply.github.com>
2024-06-05 15:01:12 +0300

    Update logout.php

--- signup/logout.php
+++ signup/logout.php
@@ -1,13 +1,34 @@
 <?php
+include ("connect.php");
 session_start();
+if (isset($_SESSION['user_id']) || isset($_SESSION['email'])) {
+    // update the is_active column to 0
+    $SQL = "UPDATE users SET is_active = 0 WHERE user_id = '" . $_SESSION['user_id'] . "' OR email = '" . $_SESSION['email'] . "'";
+    $result = $conn->query($SQL);
 
 unset($_SESSION['user_id']);
 unset($_SESSION['email']);
 unset($_SESSION['token']);
 unset($_SESSION['token_expiration']);
 
+
+
 session_destroy();
 
 header('Location: login.php');
 exit();
+
+}else{
+unset($_SESSION['user_id']);
+unset($_SESSION['email']);
+unset($_SESSION['token']);
+unset($_SESSION['token_expiration']);
+
+
+
+session_destroy();
+
+header('Location: login.php');
+exit();
+}
 ?>
