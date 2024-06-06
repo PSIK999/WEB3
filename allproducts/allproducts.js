be069572bc3e096afc699fed9c211663b569a35e
Sergio Njeim <70179273+freehotspot@users.noreply.github.com>
2024-06-06 22:44:51 +0300

    Update allproducts.js

--- allproducts/allproducts.js
+++ allproducts/allproducts.js
@@ -21,3 +21,21 @@
 // divs.forEach(div => container.appendChild(div));
 
 // }
+$('#add-to-cart-button').click(function() {
+    // Add the item to the cart
+    //...
+  
+    // Update the cart count
+    updateCartCount();
+  });
+  function updateCartCount() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'update-cart-count.php', true);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var count = xhr.responseText;
+        document.getElementById('cart-count').innerHTML = '(' + count + ')';
+      }
+    };
+    xhr.send();
+  }
