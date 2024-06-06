11fbfaa0c7ea912d733073e56f53c52395bb4615
Sergio Njeim <70179273+freehotspot@users.noreply.github.com>
2024-06-06 22:37:42 +0300

    Update index.php

--- mainPage/index.php
+++ mainPage/index.php
@@ -229,7 +229,7 @@ include "../products/productFetcher.php";
   <?php
   include("../footer/footer.php")
   ?>
-
+<script src="../cart/cart.js"></script>
   <script src="../mainPage/index.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
