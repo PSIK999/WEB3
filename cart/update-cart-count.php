ea8dd59f2f60150e30eee23b9b798e910c2cf396
Sergio Njeim <70179273+freehotspot@users.noreply.github.com>
2024-06-06 22:42:11 +0300

    Update update-cart-count.php

--- cart/update-cart-count.php
+++ cart/update-cart-count.php
@@ -1,1 +1,10 @@
+<?php
+$stmt = $conn->prepare("SELECT COUNT(*) AS num_rows FROM shoppingcart WHERE user_id =?");
+$stmt->bind_param("i", $_SESSION['user_id']);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+$num_rows = $row['num_rows'];
 
+echo $num_rows;
+?>
