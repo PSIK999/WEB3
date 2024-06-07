8686784076596e53e9f8382505a23d2c38fc21b9
Sergio Njeim <70179273+freehotspot@users.noreply.github.com>
2024-06-07 18:10:50 +0300

    Update update.php

--- adminPanel/update.php
+++ adminPanel/update.php
@@ -1,10 +1,9 @@
 <?php
-$host = "mysql-3974ab90-sergionjeim-e9a0.d.aivencloud.com:22356";
-$user = "avnadmin";
-$pass = "AVNS_lb6GguxdpmlAqBN023C";
-$db = "saheltronix";
+include "../signup/connect.php";
+require_once '../signup/admin_auth.php';
 
-$conn = new mysqli($host, $user, $pass, $db);
+
+
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
