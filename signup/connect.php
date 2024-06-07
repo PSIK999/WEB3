0e1aaf237bb349e0401cc6be09fa4bee12acb9d4
Sergio Njeim <70179273+freehotspot@users.noreply.github.com>
2024-06-07 18:03:38 +0300

    Update connect.php

--- signup/connect.php
+++ signup/connect.php
@@ -1,16 +1,4 @@
 <?php
-/*$dsn = "mysql:host=localhost;dbname=saheltronixs01";
-$dbusername = "root";
-$dbpassword = "";
-
-try {
-    $pdo = new PDO($dsn, $dbusername, $dbpassword);
-    echo "connected";
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e){
-    echo "Connection failed: " .$e->getMessage();
-}
-*/
 $host="mysql-3974ab90-sergionjeim-e9a0.d.aivencloud.com";
 $port = "22356";
 $user="avnadmin";
