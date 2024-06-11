0eb7a23dd2b3b580cf7fc5cf5d697a1bbc8ba79d
TaxEvaderer <164672360+TaxEvaderer@users.noreply.github.com>
2024-06-11 12:47:08 +0300

    Update chart1.js

--- adminPanel/assets/js/chart1.js
+++ adminPanel/assets/js/chart1.js
@@ -1,8 +1,1 @@
-const xArray = ["Laptops", "Monitors", "Accessories", "Desktops"];
-const yArray = [55 , 49, 44, 24,];
-
-const layout = { title:"Our Sales in 2024" };
-
-const data = [{ labels: xArray, values: yArray, hole: .4, type: "pie" }];
-
-Plotly.newPlot("myPlot", data, layout);
+=
