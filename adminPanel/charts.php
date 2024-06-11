accaea7e3ed456a0aecfbc70f84f90e98b252d47
TaxEvaderer <164672360+TaxEvaderer@users.noreply.github.com>
2024-06-11 12:43:42 +0300

    Update charts.php

--- adminPanel/charts.php
+++ adminPanel/charts.php
@@ -9,14 +9,14 @@ require_once '../signup/admin_auth.php';
 <!DOCTYPE html>
 <html>
 
-<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Charts</title>
     <!-- ======= Styles ====== -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+    </script>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
@@ -114,22 +114,71 @@ require_once '../signup/admin_auth.php';
                 </div>
             </div>
 
-            <div class="details">
-                <div class="myGraph">
-                    <div id="myPlot">
-                        <script src="assets/js/chart1.js">
-                        </script>
-                    </div>
+            <div class="myGraph">
+                <canvas id="myChart1" style="width:100%;max-width:700px"></canvas>
+            </div>
+            <div class="myGraph">
+                <div id="myPlot">
+                    <canvas id="myChart2" style="width:100%;max-width:1200px"></canvas>
                 </div>
-
-                <script src="assets/js/chart2.js">
-                </script>
             </div>
         </div>
-    </div>
-    <script src="assets/js/main.js"></script>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+        <script src="assets/js/main.js"></script>
+        <script>
+            var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+            var yValues = [55, 49, 44, 24, 15];
+            var barColors = [
+                "#b91d47",
+                "#00aba9",
+                "#2b5797",
+                "#e8c3b9",
+                "#1e7145"
+            ];
+
+            new Chart("myChart1", {
+                type: "doughnut",
+                data: {
+                    labels: xValues,
+                    datasets: [{
+                        backgroundColor: barColors,
+                        data: yValues
+                    }]
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: "This Week's Sales"
+                    }
+                }
+            });
+        </script>
+        <script>
+            var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+            var yValues = [55, 49, 44, 24, 15];
+            var barColors = ["red", "green", "blue", "orange", "brown"];
+
+            new Chart("myChart2", {
+                type: "bar",
+                data: {
+                    labels: xValues,
+                    datasets: [{
+                        backgroundColor: barColors,
+                        data: yValues
+                    }]
+                },
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: true,
+                        text: "This Year's Sales"
+                    }
+                }
+            });
+        </script>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
 
 </body>
